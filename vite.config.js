c26459dd89c3ab71f16ca9d09da2084fe1398604
amardeep10 <amardeep@shadowcast.io>
2025-09-05 17:09:27 +0530

    base change

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/explore-taj-holidays/", // Important: matches repo name
+  // base: "/explore-taj-holidays/", // Important: matches repo name
 });
